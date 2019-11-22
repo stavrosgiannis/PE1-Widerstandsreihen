54f7a3b083e4e40252c9bbe875707cbafa124078
Stav ros <hackerone1@web.de>
2019-11-22 17:00:04 +0100

    17:00 22/11/2019

--- PE1_Widerstandsreihen/PE1_Widerstandsreihen.c
+++ PE1_Widerstandsreihen/PE1_Widerstandsreihen.c
@@ -4,8 +4,6 @@
 #include <stdio.h>
 #include <math.h>
 
-double e_series;
-
 int color_ring1[] = {
 	/*black =*/ 0,
 	/*brown =*/ 1,
@@ -65,9 +63,45 @@ double compute_tolerance(int INPUT_E_SERIES) {
 	}
 }
 
-char* check_color(int arr[100], int n, int e_series) {
-	if (e_series <= 24) {
-		for (size_t i = 0; i < 100; i++)
+char* check_color(int arr[100], int n, int INPUT_E_SERIES) {
+	for (size_t i = 0; i < 100; i++)
+	{
+		if (INPUT_E_SERIES <= 24)
+		{
+			if (n == 0)
+			{//color ring 1
+				switch (arr[i])
+				{
+				case 0: return("\tblack");
+				case 1:	return("\tbrown");
+				case 2:	return("\tred");
+				case 3:	return("\torange");
+				case 4:	return("\tyellow");
+				case 5:	return("\tgreen");
+				case 6:	return("\tblue");
+				case 7:	return("\tpurple");
+				case 8:	return("\tgrey");
+				case 9:	return("\twhite");
+				}
+			}
+			else if (n == 1)
+			{//color ring 2
+				switch (arr[i])
+				{
+				case 0: return("\tblack");
+				case 1:	return("\tbrown");
+				case 2:	return("\tred");
+				case 3:	return("\torange");
+				case 4:	return("\tyellow");
+				case 5:	return("\tgreen");
+				case 6:	return("\tblue");
+				case 7:	return("\tpurple");
+				case 8:	return("\tgrey");
+				case 9:	return("\twhite");
+				}
+			}
+		}
+		else if (INPUT_E_SERIES > 24)
 		{
 			if (n == 0) {//color ring 1
 				switch (arr[i])
@@ -84,7 +118,8 @@ char* check_color(int arr[100], int n, int e_series) {
 				case 9:	return("\twhite");
 				}
 			}
-			else if (n == 1) {//color ring 1
+			else if (n == 1)
+			{//color ring 2
 				switch (arr[i])
 				{
 				case 0: return("\tblack");
@@ -99,7 +134,8 @@ char* check_color(int arr[100], int n, int e_series) {
 				case 9:	return("\twhite");
 				}
 			}
-			else if (n == 2) {
+			else if (n == 2)
+			{//color ring 3
 				switch (arr[i])
 				{
 				case 0: return("\tblack");
@@ -109,17 +145,9 @@ char* check_color(int arr[100], int n, int e_series) {
 				case 4:	return("\tyellow");
 				case 5:	return("\tgreen");
 				case 6:	return("\tblue");
-				case 8:	return("\tgold");
-				case 9:	return("\tsilver");
-				}
-			}
-			else if (n == 3) {
-				switch (arr[i])
-				{
-				case 1:	return("\tbrown");
-				case 2:	return("\tred");
-				case 8:	return("\tgold");
-				case 9:	return("\tsilver");
+				case 7:	return("\tpurple");
+				case 8:	return("\tgrey");
+				case 9:	return("\twhite");
 				}
 			}
 		}
@@ -172,8 +200,8 @@ unsigned int count(unsigned int i) {
 	return ret;
 }
 
-int print_first_digit(double table[192][3]) {
-	int num = (int)table[20][1]; //for example
+int print_digits(double table[192][3], double INPUT_E_SERIES) {
+	int num = (int)table[16][1]; //for example
 	int dig = count(num);
 	int arr[100];
 
@@ -181,23 +209,83 @@ int print_first_digit(double table[192][3]) {
 		arr[dig] = num % 10;
 		num /= 10;
 		printf("\n%d \tZahlenstelle: %d", arr[dig], dig + 1);
-
-		if (dig == 0/*Erste Ziffer*/) {
-			printf("\t%s", check_color(arr, 0, e_series));
+		if (INPUT_E_SERIES <= 24) {
+			if (dig == 0/*Erste Ziffer*/) {
+				printf("\t%s", check_color(arr, 0, INPUT_E_SERIES));
+			}
+			else if (dig == 1/*Zweite Ziffer*/) {
+				printf("\t%s", check_color(arr, 1, INPUT_E_SERIES));
+				/*test_function(arr[1]);*/
+			}
+			else if (dig == 2/*Dritte Ziffer*/) {
+			}
+			else if (dig == 3/*Vierte Ziffer*/) {
+			}
 		}
-		else if (dig == 1/*Zweite Ziffer*/) {
-			printf("\t%s", check_color(arr, 1, e_series));
-		}
-		else if (dig == 2/*Zweite Ziffer*/) {
-			printf("\t%s", check_color(arr, 2, e_series));
-		}
-		else if (dig == 3/*Zweite Ziffer*/) {
-			printf("\t%s", check_color(arr, 3, e_series));
+		else if (INPUT_E_SERIES > 24) {
+			if (dig == 0/*Erste Ziffer*/) {
+				printf("\t%s", check_color(arr, 0, INPUT_E_SERIES));
+			}
+			else if (dig == 1/*Zweite Ziffer*/) {
+				printf("\t%s", check_color(arr, 1, INPUT_E_SERIES));
+			}
+			else if (dig == 2/*Dritte Ziffer*/) {
+				printf("\t%s", check_color(arr, 1, INPUT_E_SERIES));
+			}
+			else if (dig == 3/*Vierte Ziffer*/) {
+			}
 		}
 	}
 
 	//int size = sizeof(arr) / sizeof(arr[0]);//Method
 	//printf("\nSize of Array: %d", size);
+}
+
+char* getToleranceRing(int INPUT_E_SERIES) {
+	printf("\n\tTolerance Ring:");
+	switch (INPUT_E_SERIES)
+	{
+	case 12:
+		return "\tsilver";
+	case 24:
+		return "\tyellow";
+	case 48:
+		return "\tred";
+	case 96:
+		return "\tbrown";
+	default:
+		break;
+	}
+}
+
+int getMultiplierRing(int INPUT_decade) {
+	printf("\n\tMultiplier Ring:");
+	switch (INPUT_decade)
+	{
+	case 0:
+		printf("\tBlack");
+		break;
+	case 1:
+		printf("\tBrown");
+		break;
+	case 2:
+		printf("\tRed");
+		break;
+	case 3:
+		printf("\torange");
+		break;
+	case 4:
+		printf("\tyellow");
+		break;
+	case 5:
+		printf("\tgreen");
+		break;
+	case 6:
+		printf("\tblue");
+		break;
+	}
+
+	return 0;
 }
 
 // Konstanten fuer den Zugriff in der Tabelle
@@ -214,6 +302,19 @@ double compute_values(double e_series, int decade, double tolerance, double tabl
 		table[i][LOWER_BOUND] = table[i][SET_VALUE] - (table[i][SET_VALUE] * tolerance);
 		table[i][UPPER_BOUND] = table[i][SET_VALUE] + (table[i][SET_VALUE] * tolerance);
 	}
+	return 0;
+}
+
+int test_function(int faktor) {
+	double runde_zahl_auf = faktor;
+	double neue_runde_zahl = runde_zahl_auf;
+
+	while (neue_runde_zahl <= runde_zahl_auf + 1)
+	{
+		neue_runde_zahl = neue_runde_zahl + 0.00001;
+	}
+
+	printf("\n\t%d", (int)neue_runde_zahl);
 	return 0;
 }
 
@@ -247,7 +348,11 @@ int main()
 		compute_coding(e_series, table[n][SET_VALUE], tolerance, coding);
 		printf("Die Farbcodierung dieses Widerstands ist: %s\n", coding);*/
 
-		print_first_digit(table);
+		print_digits(table, e_series);
+
+		getMultiplierRing(decade, e_series);
+
+		printf("%s", getToleranceRing(e_series));
 
 		printf("\n\n\terneute Berechnung? (j/n)\n\t");
 	} while (getchar() == 'j' || getchar() == 'J');
